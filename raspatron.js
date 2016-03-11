01eb9c3f0fc6b71a2780f05a67523f6ca85685ca
Kenneth Centurion <kcenturion@yahoo.com>
2016-03-11 14:58:18 -0600

    tewt

--- raspatron.js
+++ raspatron.js
@@ -1,117 +1,147 @@
 var nodatron = require('./node_modules/nodatron/lib/nodatron.js'),
+    pathUtil = require('path'),
+    log      = require(pathUtil.join(__dirname,'./logger.js')),
     cp       = require('child_process');
 
 var arduino = new nodatron({"device" : "/dev/ttyACM0","baud" : 9600});
 
+arduino.enableConsole();
+
 arduino.on("connected", function(){
-var powerLed          = arduino.createLed(2);
-var activityLed       = arduino.createLed(3);
-var motionLed         = arduino.createLed(4);
-var connectivityLed   = arduino.createLed(5);
-var powerButton       = arduino.createButton(6);
-var motionSensorFront = arduino.createPir(7);
-var motionSensorLeft  = arduino.createPir(8);
-var motionSensorRight = arduino.createPir(11);
-var baseServo         = arduino.createServo(9);
-var camServo          = arduino.createServo(10);
+  var powerLed          = arduino.createLed(2);
+  var activityLed       = arduino.createLed(3);
+  var motionLed         = arduino.createLed(4);
+  var connectivityLed   = arduino.createLed(5);
+  var powerButton       = arduino.createButton(6);
+  var motionSensorFront = arduino.createPir(7);
+  var motionSensorLeft  = arduino.createPir(8);
+  var motionSensorRight = arduino.createPir(11);
+  var baseServo         = arduino.createServo(9);
+  var camServo          = arduino.createServo(10);
 
-powerButton.on('on',function(){
-powerLed.turnOn();
-panCenter();
+  powerButton.on('on',function(){
+    powerLed.turnOn();
+    panCenter();
 
-executeCommand("/usr/local/src/startStream.sh",function(result,msg){
-  if(result){
-    console.info(msg);
+    executeCommand("/usr/local/src/startStream.sh",function(result,msg){
+      if(result){
+        console.info(msg);
+      }
+      else{
+        console.error(msg);
+      }
+    });
+  });
+
+  powerButton.on('off',function(){
+    powerLed.turnOff();
+    motionLed.turnOff();
+    connectivityLed.turnOff();
+    activityLed.turnOff();
+    sleepCam();
+
+    executeCommand("/usr/local/src/stopStream.sh",function(result,msg){
+      if(result){
+        console.info(msg);
+      }
+      else{
+        console.error(msg);
+      }
+    });
+  });
+
+  motionSensorFront.on('start',function(){
+    if(powerButton.isOn()){
+      motionLed.startBlink();
+      panCenter();
+    }
+  });
+
+  motionSensorFront.on('stop',function(){
+    motionLed.stopBlink();
+  });
+
+  motionSensorLeft.on('start',function(){
+    if(powerButton.isOn()){
+      motionLed.startBlink();
+      panLeft();
+    }
+  });
+
+  motionSensorLeft.on('stop',function(){
+  motionLed.stopBlink();
+  });
+
+  motionSensorRight.on('start',function(){
+    if(powerButton.isOn()){
+      motionLed.startBlink();
+      panRight();
+    }
+  });
+
+  motionSensorRight.on('stop',function(){
+    motionLed.stopBlink();
+  });
+
+  function panLeft(){
+    baseServo.move(180);
+    camServo.move(85);
   }
-  else{
-    console.error(msg);
+
+  function panRight(){
+    baseServo.move(33);
+    camServo.move(85);
   }
-});
-});
 
-powerButton.on('off',function(){
-powerLed.turnOff();
-motionLed.turnOff();
-connectivityLed.turnOff();
-activityLed.turnOff();
-sleepCam();
-
-executeCommand("/usr/local/src/stopStream.sh",function(result,msg){
-  if(result){
-    console.info(msg);
+  function panCenter(){
+    baseServo.move(110);
+    camServo.move(85);
   }
-  else{
-    console.error(msg);
+
+  //move camera to a rest position.
+  function sleepCam(){
+    baseServo.move(110);
+    camServo.move(0);
   }
-});
-});
 
-motionSensorFront.on('start',function(){
-if(powerButton.isOn()){
-  motionLed.startBlink();
-  panCenter();
-}
-});
-
-motionSensorFront.on('stop',function(){
-motionLed.stopBlink();
-});
-
-motionSensorLeft.on('start',function(){
-if(powerButton.isOn()){
-  motionLed.startBlink();
-  panLeft();
-}
-});
-
-motionSensorLeft.on('stop',function(){
-motionLed.stopBlink();
-});
-
-motionSensorRight.on('start',function(){
-if(powerButton.isOn()){
-  motionLed.startBlink();
-  panRight();
-}
-});
-
-motionSensorRight.on('stop',function(){
-motionLed.stopBlink();
-});
-
-function panLeft(){
-baseServo.move(180);
-camServo.move(85);
-}
-
-function panRight(){
-baseServo.move(33);
-camServo.move(85);
-}
-
-function panCenter(){
-baseServo.move(110);
-camServo.move(85);
-}
-
-//move camera to a rest position.
-function sleepCam(){
-baseServo.move(110);
-camServo.move(0);
-}
-
-function executeCommand(cmd,cb){
-var child = cp.exec(cmd ,function(error,stdout,stderr){
-  if (stderr){
-    cb(false,"Error executing command "+cmd+" with stderr:"+stderr);
+  function executeCommand(cmd,cb){
+    var child = cp.exec(cmd ,function(error,stdout,stderr){
+      if (stderr){
+        cb(false,"Error executing command "+cmd+" with stderr:"+stderr);
+      }
+      if (error) {
+        cb(false,"Error executing command "+cmd+" with error:"+error);
+      }
+      if(stdout){
+        cb(true,cmd+" successfully executed with no errors.",stdout);
+      }
+    });
   }
-  if (error) {
-    cb(false,"Error executing command "+cmd+" with error:"+error);
-  }
-  if(stdout){
-    cb(true,cmd+" successfully executed with no errors.",stdout);
-  }
-});
-}
+
+
+  //define handlers for this main process.
+  process.on('SIGTERM', function() {//called from /etc/init.d/nodatron.sh from kill pid
+    log.info("Got kill signal. Exiting.");
+    arduino.shutdown();
+  });
+
+  process.on('SIGINT', function() {
+    log.warn("Caught interrupt signal(Ctrl-C)");
+    arduino.shutdown();
+  });
+
+  process.on('exit', function(){
+    log.info("raspatron exiting...");
+  })
+
+  process.on('uncaughtException', function (err) {
+    var msg="Uncaught Exception ";
+    if( err.name === 'AssertionError' ) {
+      msg += err.message;
+    } else {
+      msg += err;
+    }
+    log.error(msg);
+  });
 
 });
--- update.sh
+++ update.sh
@@ -3,40 +3,22 @@
 #
 # note: this script does NOT update the Arduino custom libraries. Move those yourself.
 
-SRC_DIR = "/usr/local/src"
-TMP_DIR = "/tmp/raspatron"
-
 clear
 
 echo "Executing update script..."
 
-echo "Deleting old tmp dir" $TMP_DIR
-rm -rf $TMP_DIR
-
-echo "Deleting old raspatron dir"
-rm -rf $SRC_DIR/raspatron
-
-echo "Re-creating tmp dir " $TMP_DIR
-mkdir $TMP_DIR
-
-echo "Retrieving latest repo: git clone https://github.com/zoroloco/raspatron.git " $TMP_DIR
-git clone https://github.com/zoroloco/raspatron.git $TMP_DIR
-
-echo "Moving latest code to " $SRC_DIR
-cp -R $TMP_DIR $SRC_DIR
-
-echo "Cleaning up " $TMP_DIR
-rm -rf $TMP_DIR
+rm -rf /usr/local/src/raspatron
+git clone https://github.com/zoroloco/raspatron.git /usr/local/src/raspatron
 
 echo "Now building latest Raspatron."
-cd $SRC_DIR/raspatron/arduino
+cd /usr/local/src/raspatron/arduino
 ino build
-sleep 5
-echo "Uploading Raspatron to Arduino."
-ino upload
+#sleep 5
+#echo "Uploading Raspatron to Arduino."
+#ino upload
 
 echo "Now installing dependencies."
-cd $SRC_DIR/raspatron
+cd /usr/local/src/raspatron
 sudo npm install
 
 echo "Now running raspatron."
