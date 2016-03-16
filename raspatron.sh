8b7ce75a247b4a4c33b8978457a872cf698824e6
Kenneth Centurion <kcenturion@yahoo.com>
2016-03-16 13:16:10 -0500

    start stream fixed

--- raspatron.sh
+++ raspatron.sh
@@ -11,7 +11,8 @@ if pgrep mjpg_streamer > /dev/null
 then
   echo "mjpg_streamer already running"
 else
-  LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_raspicam.so -fps 15 -q 50 -x 640 -y 480" -o "output_http.so -p 8171 -w /opt/mjpg-streamer/www -c $
+  LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_raspicam.so -fps 15 -q 50 -x 980 -y 800" -o "output_http.so -p 8171 -w /opt/mjpg-streamer/www" > /dev/null 2>&1&
+
   echo "mjpg_streamer started"
 fi
 
